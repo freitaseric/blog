a2c11b20ba87c4bcc7833ed769f3a5b9c85cbf31
freitaseric <contato@freitaseric.com>
2025-10-15 13:11:13 -0400

    feat(ci): migrate to remote database

--- config/runtime.exs
+++ config/runtime.exs
@@ -10,7 +10,7 @@ if config_env() == :prod do
       raise "environment variable DATABASE_URL is missing"
 
   config :blog, Blog.Repo,
-    ssl: false,
+    ssl: true,
     url: database_url,
     pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
 
