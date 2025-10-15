09957a38ef3b45b5245dfe7aa21357fd1cda3a78
freitaseric <contato@freitaseric.com>
2025-10-15 14:54:14 -0400

    fix(runtime): configure the new way to connect to database

--- config/runtime.exs
+++ config/runtime.exs
@@ -5,14 +5,13 @@ if System.get_env("PHX_SERVER") do
 end
 
 if config_env() == :prod do
-  database_url =
-    System.get_env("DATABASE_URL") ||
-      raise "environment variable DATABASE_URL is missing"
-
   config :blog, Blog.Repo,
-    ssl: true,
-    url: database_url,
-    pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
+    username: System.get_env("DB_USERNAME", "postgres"),
+    password: System.get_env("DB_PASSWORD", "postgres"),
+    hostname: System.get_env("DB_HOSTNAME", "localhost"),
+    database: System.get_env("DB_NAME", "blog_db"),
+    pool_size: 10,
+    ssl: String.to_atom(System.get_env("DB_SSL", "false"))
 
   secret_key_base =
     System.get_env("SECRET_KEY_BASE") ||
